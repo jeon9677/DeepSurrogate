b60b4f31d7319f4ea2e53806bb8bd67193819c8c
Yeseul, Jeon <70432016+jeon9677@users.noreply.github.com>
2025-07-07 16:18:31 -0500

    Update models.py

--- DeepSurrogate_model/models.py
+++ DeepSurrogate_model/models.py
@@ -1,1 +1,82 @@
 
+import tensorflow as tf
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Dense, Dropout, Multiply, Add, Lambda, concatenate
+
+def get_dropout(x, p=0.5, mc=False):
+    return Dropout(p)(x, training=mc)
+
+def get_model_deepsurrogate(
+    global_dim=5,
+    spatial_dim=2,
+    local_dim=1,
+    global_hidden=[16, 8],
+    spatial_hidden=[16, 8],
+    noise_hidden=[32, 16],
+    dropout_p=0.1,
+    mc=False,
+    final_act="softplus"
+):
+    """
+    
+    Args:
+        global_dim (int): dimension of global feature 
+        spatial_dim (int):  dimension of spatial coordinates 
+        local_dim (int): dimension of local feature 
+        global_hidden (list): 글로벌 term Dense layer units
+        spatial_hidden (list): spatial term Dense layer units
+        noise_hidden (list): noise branch Dense layer units
+        dropout_p (float): dropout rate
+        mc (bool): Monte Carlo Dropout 
+        final_act (str): activation
+
+    Returns:
+        Compiled Keras model
+    """
+
+    inp_global = Input(shape=(global_dim,), name='global_inp')
+    s_input = Input(shape=(spatial_dim,), name='spatial')
+    local_input = Input(shape=(local_dim,), name='local')
+
+    # Global term
+    x = inp_global
+    for units in global_hidden:
+        x = Dense(units, activation='relu')(x)
+        x = get_dropout(x, p=dropout_p, mc=mc)
+
+    # Spatial eta term
+    eta = s_input
+    for units in spatial_hidden:
+        eta = Dense(units, activation='relu')(eta)
+        eta = get_dropout(eta, p=dropout_p, mc=mc)
+
+    # Combine
+    B_eta = Multiply(name='basis_eta_product')([x, eta])
+    B_eta_sum = Lambda(lambda x: tf.reduce_sum(x, axis=1, keepdims=True), name='sum_B_eta')(B_eta)
+
+    final = concatenate([B_eta_sum, local_input])
+    out = Dense(1, activation=final_act)(final)
+
+    # Log-sigma^2 noise term
+    log_sigma2 = s_input
+    for units in noise_hidden:
+        log_sigma2 = Dense(units, activation='softplus')(log_sigma2)
+        log_sigma2 = get_dropout(log_sigma2, p=dropout_p, mc=mc)
+    log_sigma2 = Dense(1, activation='linear', name='log_sigma2')(log_sigma2)
+    log_sigma2_clipped = Lambda(lambda x: tf.clip_by_value(x, -10, 10), name='log_sigma2_clipped')(log_sigma2)
+
+    noise = Lambda(lambda x: tf.exp(x), name='lognormal_noise')(log_sigma2_clipped)
+
+    final_output = Add(name='final_output')([out, noise])
+
+    model = Model(inputs=[inp_global, s_input, local_input], outputs=final_output)
+
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=1e-2,
+        decay_steps=1000,
+        decay_rate=0.96
+    )
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+    model.compile(optimizer=optimizer, loss='mse', metrics=['mse'])
+
+    return model
