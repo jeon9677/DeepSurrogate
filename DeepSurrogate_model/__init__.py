ffa8b6f0f4d54aee8c6168c5611da0143436a37b
Yeseul, Jeon <70432016+jeon9677@users.noreply.github.com>
2026-07-09 11:04:36 -0500

    Update __init__.py

--- DeepSurrogate_model/__init__.py
+++ DeepSurrogate_model/__init__.py
@@ -1,8 +1,9 @@
-from .models import get_model_deepsurrogate
+from .models import get_model_deepsurrogate, gaussian_nll
 from .train import train_model, mc_predict
 
 __all__ = [
     "get_model_deepsurrogate",
+    "gaussian_nll",
     "train_model",
     "mc_predict",
 ]
