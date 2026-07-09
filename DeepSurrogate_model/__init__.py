2b084c3411bc0fda71310f6eed845862e2ea06c3
Yeseul, Jeon <70432016+jeon9677@users.noreply.github.com>
2026-07-09 11:03:50 -0500

    Update __init__.py to include model functions
    Added model and training functions to module exports.

--- DeepSurrogate_model/__init__.py
+++ DeepSurrogate_model/__init__.py
@@ -1,1 +1,10 @@
+from .models import get_model_deepsurrogate
+from .train import train_model, mc_predict
 
+__all__ = [
+    "get_model_deepsurrogate",
+    "train_model",
+    "mc_predict",
+]
+
+__version__ = "0.1.0"
